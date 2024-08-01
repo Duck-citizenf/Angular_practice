bde49b895e11e396a62d68eae2db61a5894c980c
Duck <bogdanovvv@elmeh.ru>
2024-08-01 13:19:22 +0300

    аа

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   template: `<h1>Hello World!</h1>`,
   styleUrls: ['./app.component.css'],
-})
-export class AppComponent {
-  title = 'homes';
-}
-
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-
-@Component ({
   imports: [
     MatSlideToggleModule,
   ]
 })
-export class AppComponent2 {}
+export class AppComponent {
+  
+}
+
 // https://material.angular.io/guide/getting-started
