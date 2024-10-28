334f50453ef7cca09099ba82889b1ce7e019b705
Duck <bogdanovvv@elmeh.ru>
2024-10-28 15:45:55 +0300

    сайдбар

--- src/tabs/tabs.ts
+++ src/tabs/tabs.ts
@@ -6,18 +6,20 @@ import {MatBadgeModule} from '@angular/material/badge';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 @Component({
     selector: 'whole-component',
     styleUrls: ['badge.overview.css'],
-    templateUrl: 'index.html',
+    templateUrl: 'tabindex.html',
     standalone: true,
-    imports: [MatTabsModule, MatBadgeModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule],
+    imports: [MatTabsModule, MatBadgeModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatSidenavModule],
 })
 
 export class WholeComponent {
     hidden = false;
+    showFiller = false;
 
     toggleBadgeVisibility() {
       this.hidden = !this.hidden;
